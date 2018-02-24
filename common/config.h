2e4d298a951624a1de21b23fb77b0a95ceb3e1dc
Rajit Manohar <rajit.manohar@yale.edu>
2018-02-24 17:11:37 -0500

    added prefix support to config file variables start of layout library

--- common/config.h
+++ common/config.h
@@ -14,38 +14,42 @@ extern "C" {
 
 /* append to config search path; the last call contains the path with
    the highest priority */
-void config_append_path (char *s);
+void config_append_path (const char *s);
 
 /* Add standard configuration path
    .  <- highest
    $ACT_HOME/lib/tool/ <- next
    $CAD_HOME/lib/tool/ <- next
 */
-void config_std_path (char *tool);
+void config_std_path (const char *tool);
 
 /* read configuration file */
-void config_read (char *s);
+void config_read (const char *s);
+
+/* set prefix for the configuration */
+void config_push_prefix (const char *s);
+void config_pop_prefix (void);
 
 /* read parameter from configuration file (or default value) */
-int config_get_int (char *s);
-double config_get_real (char *s);
-char *config_get_string (char *s);
+int config_get_int (const char *s);
+double config_get_real (const char *s);
+char *config_get_string (const char *s);
 
 /* table read functions */
-int config_get_table_size (char *s);
-double *config_get_table_real (char *s);
-int *config_get_table_int (char *s);
+int config_get_table_size (const char *s);
+double *config_get_table_real (const char *s);
+int *config_get_table_int (const char *s);
 
 /* set default value */
-void config_set_default_int (char *s, int v);
-void config_set_default_real (char *s, double v);
-void config_set_default_string (char *s, char *t);
+void config_set_default_int (const char *s, int v);
+void config_set_default_real (const char *s, double v);
+void config_set_default_string (const char *s, char *t);
 
 /* clear configuration tables */
 void config_clear (void);
 
 /* check if variable exists */
-int config_exists (char *s);
+int config_exists (const char *s);
 
 #ifdef __cplusplus
 }
