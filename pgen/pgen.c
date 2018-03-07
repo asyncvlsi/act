7dd517ccb14f6c130fe48952c07a81f72ee29ae1
Rajit Manohar <rajit.manohar@yale.edu>
2018-03-07 15:35:52 -0500

    fixed a number of minor bugs

--- pgen/pgen.c
+++ pgen/pgen.c
@@ -1915,6 +1915,7 @@ void emit_parser (void)
   pp_puts (pp, " fprintf (stderr, \"File `%s', line %d, col %d\\n\\t\", p->f, p->l, p->c);"); pp_nl;
   pp_puts (pp, " va_start (ap, fmt);"); pp_nl;
   pp_puts (pp, " vfprintf (stderr, fmt, ap);"); pp_nl;
+  pp_puts (pp, " fprintf (stderr, \"\\n\");"); pp_nl;
   pp_puts (pp, " va_end (ap);"); pp_nl;
   pp_puts (pp, "}"); pp_nl; pp_nl;
 
