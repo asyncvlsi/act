2c53ee03c523496cbdd4bccef790e16752626beb
Rajit Manohar <rajit.manohar@yale.edu>
2018-08-09 14:27:42 -0400

    use aflat cmd-line removed redundant prototype

--- aflat/main.cc
+++ aflat/main.cc
@@ -9,10 +9,7 @@
 #include <string.h>
 #include "aflat.h"
 
-enum output_formats {
-  PRSIM_FMT,
-  LVS_FMT
-};
+output_formats export_format;
 
 void usage (char *s)
 {
@@ -24,17 +21,18 @@ int main (int argc, char **argv)
 {
   Act *a;
   char *file;
-  output_formats opt;
 
   Act::Init (&argc, &argv);
+  
+  export_format = PRSIM_FMT;
 
   if (argc > 3) usage(argv[0]);
   if (argc == 3) {
     if (strcmp (argv[1], "-prsim") == 0) {
-      opt = PRSIM_FMT;
+      export_format = PRSIM_FMT;
     }
     else if (strcmp (argv[1], "-lvs") == 0) {
-      opt = LVS_FMT;
+      export_format = LVS_FMT;
     }
     else {
       usage (argv[0]);
