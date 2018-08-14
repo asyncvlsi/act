7c91520d8758cc8cbc8c4d47ae33ecc57b3e7cb2
Rajit Manohar <rajit.manohar@yale.edu>
2018-08-14 10:40:44 -0400

    passes directory added

---
+++ passes/passes.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+ *
+ *  Copyright (c) 2018 Rajit Manohar
+ *  All Rights Reserved
+ *
+ **************************************************************************
+ */
+#ifndef __ACT_PASSES_H__
+#define __ACT_PASSES_H__
+
+#include <act/act.h>
+
+void act_prsflat_prsim (Act *a);
+void act_prsflat_lvs (Act *a);
+void act_expand (Act *a);
+
+
+#endif /* __ACT_PASSES_H__ */
