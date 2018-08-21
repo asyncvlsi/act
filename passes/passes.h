7e1971f1d337c404aafd5fe62dce2f9514614180
Rajit Manohar <rajit.manohar@yale.edu>
2018-08-21 15:54:34 -0400

    more passes

--- passes/passes.h
+++ passes/passes.h
@@ -10,9 +10,15 @@
 
 #include <act/act.h>
 
-void act_prsflat_prsim (Act *a);
-void act_prsflat_lvs (Act *a);
 void act_expand (Act *a);
 
+void act_prsflat_prsim (Act *a);
+void act_prsflat_lvs (Act *a);
+
+void act_flat_apply_conn_pairs (Act *a, void *cookie,
+				void (*f)(void *c, ActId *one, ActId *two));
+
+void act_flat_apply_processes (Act *a, void *cookie,
+			       void (*f)(void *c, ActId *name, Process *p));
 
 #endif /* __ACT_PASSES_H__ */
