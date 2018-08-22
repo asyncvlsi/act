a2ac7bbd83b52f9ed3f690d42540cb2f5408703a
Rajit Manohar <rajit.manohar@yale.edu>
2018-08-22 09:31:42 -0400

    restructed directory, changed netlist generator to use config file for parameter passing rather than global variables

--- transform/prs2net/main.cc
+++ transform/prs2net/main.cc
@@ -6,93 +6,18 @@
  **************************************************************************
  */
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-#include "misc.h"
-#include "config.h"
-#include "globals.h"
+#include <string.h>
 #include <act/act.h>
-
-/* 
-   Global variables for netlist generation.
-*/
-
-int verbose;
-
-double lambda;			/* scale factor from width expressions
-				   to absolute units */
-
-int min_w_in_lambda;		/* minimum width in lambda */
-int min_l_in_lambda;		/* minimum length in lambda */
-
-int max_n_w_in_lambda;		/* maximum n-width in lambda */
-int max_p_w_in_lambda;		/* maximum p-width in lambda	*/
-
-/* standard transistor sizes */
-int std_p_width;
-int std_p_length;
-int std_n_width;
-int std_n_length;
-
-/* standard staticizer sizes */
-int stat_p_width;
-int stat_p_length;
-int stat_n_width;
-int stat_n_length;
-
-/* strength ratios */
-double p_n_ratio;
-double weak_to_strong_ratio;
-
-/* internal diffusion */
-int fet_spacing_diffonly;
-int fet_spacing_diffcontact;
-int fet_diff_overhang;
-
-int top_level_only;		/* only emit top-level cell! */
-
-int emit_parasitics;		/* emit parasitic source/drain area/perim */
-
-int black_box_mode;		/* 1 if implicit blackbox */
-
-/* load cap on each node */
-double default_load_cap;
-
-int ignore_loadcap;		/* ignore loadcap directives (lvs only) */
-
-/* discrete widths */
-double *fet_width_ranges;
-int fet_width_num;
-
-/* discrete lengths */
-int discrete_length;
-
-/* fet extra string */
-char *extra_fet_string;
-
-/* disable staticizers/keepers */
-int disable_keepers;
-
-/* swap source and drain */
-int swap_source_drain;
-
-/* use subckt models */
-int use_subckt_models;
-
-/* fold transistors */
-int fold_nfet_width;
-int fold_pfet_width;
-
+#include "config.h"
+#include "netlist.h"
 
 FILE *fpout;			/* output file */
-
 int emit_verilog;
 FILE *fvout;			/* output verilog module file */
-
 int emit_pinfo;
 FILE *fpinfo;			/* output pinfo file */
 
-int omit_printing;
 
 /*
   Check for trailing .extension
@@ -115,7 +40,7 @@ static int has_trailing_extension (const char *s, const char *ext)
 
 static void usage (char *name)
 {
-  fprintf (stderr, "Usage: %s [-vdltBR] [-C <conf>] [-p <proc>] [-o <file>] [-V <file>] <act>\n", name);
+  fprintf (stderr, "Usage: %s [-dltBR] [-C <conf>] [-p <proc>] [-o <file>] [-V <file>] <act>\n", name);
   fprintf (stderr, " -C <conf> Configuration file name\n");
   fprintf (stderr, " -t        Only emit top-level cell (no sub-cells)\n");
   fprintf (stderr, " -p <proc> Emit process <proc>\n");
@@ -124,7 +49,6 @@ static void usage (char *name)
   fprintf (stderr, " -B	       Black-box mode. Assume empty act process is an external .sp file\n");
   fprintf (stderr, " -V <file> Print Verilog module for the top level in <file> and port information on <file>.port_info \n");
   fprintf (stderr, " -l	       LVS netlist; ignore all load capacitances\n");
-  fprintf (stderr, " -v	       Increase verbosity level\n");
   exit (1);
 }
 
@@ -139,24 +63,19 @@ char *initialize_parameters (int *argc, char ***argv)
   char *proc_name;
   char *act_cmdline;
   int ch;
+  int ignore_loadcap = 0;
+  int emit_parasitics = 0;
+  int black_box_mode = 0;
+  int top_level_only = 0;
 
-  verbose = 0;
-  omit_printing = 0;
   emit_verilog = 0;
   emit_pinfo = 0;
   fpout = stdout;
   top_level_only = 0;
   conf_file = NULL;
-  emit_parasitics = 0;
-  fet_width_num = 0;
-  fet_width_ranges = NULL;
-  extra_fet_string = NULL;
-  black_box_mode = 0;
-  disable_keepers = 0;
-  ignore_loadcap = 0;
   proc_name = NULL;
 
-  while ((ch = getopt (*argc, *argv, "BdC:tp:o:RlX:V:v")) != -1) {
+  while ((ch = getopt (*argc, *argv, "BdC:tp:o:lV:")) != -1) {
     switch (ch) {
     case 'l':
       ignore_loadcap = 1;
@@ -198,10 +117,6 @@ char *initialize_parameters (int *argc, char ***argv)
       }
       break;
 
-    case 'v':
-      verbose++;
-      break;
-
     case 'V':
       if (optarg == 0) {
 	usage ((*argv)[0]);
@@ -209,9 +124,6 @@ char *initialize_parameters (int *argc, char ***argv)
       char port_info_f[1024];
       strncpy (port_info_f, optarg, 1000);
       strcat (port_info_f, ".port_info");
-      if (verbose) {
-	fprintf (stderr, "Printing Verilog module to file %s and port information in %s\n", optarg, port_info_f);
-      }
       fvout = fopen (optarg, "w");
       if (!fvout) {
 	fatal_error ("Could not open file `%s' for writing.", optarg);
@@ -300,6 +212,11 @@ char *initialize_parameters (int *argc, char ***argv)
   config_set_default_int ("fold_pfet_width", 0);
   config_set_default_int ("fold_nfet_width", 0);
 
+  config_set_default_int ("ignore_loadcap", ignore_loadcap);
+  config_set_default_int ("emit_parasitics", emit_parasitics);
+  config_set_default_int ("black_box_mode", black_box_mode);
+  config_set_default_int ("top_level_only", top_level_only);
+  
   /* read config file */
   if (has_trailing_extension (conf_file, "conf")) {
     config_read (conf_file);
@@ -310,70 +227,13 @@ char *initialize_parameters (int *argc, char ***argv)
     config_read (buf);
   }
 
-  lambda = config_get_real ("lambda");
-
-  min_w_in_lambda = config_get_int ("min_width");
-  min_l_in_lambda = config_get_int ("min_length");
-
-  max_n_w_in_lambda = config_get_int ("max_n_width");
-  max_p_w_in_lambda = config_get_int ("max_p_width");
-
-  std_p_width = config_get_int ("std_p_width");
-  std_p_length = config_get_int ("std_p_length");
-  std_n_width = config_get_int ("std_n_width");
-  std_n_length = config_get_int ("std_n_length");
-
-  stat_p_width = config_get_int ("stat_p_width");
-  stat_p_length = config_get_int ("stat_p_length");
-  stat_n_width = config_get_int ("stat_n_width");
-  stat_n_length = config_get_int ("stat_n_length");
-
-  p_n_ratio = config_get_real ("p_n_ratio");
-  weak_to_strong_ratio = config_get_real ("weak_to_strong_ratio");
-
-  fet_spacing_diffonly = config_get_int ("fet_spacing_diffonly");
-  fet_spacing_diffcontact = config_get_int ("fet_spacing_diffcontact");
-  fet_diff_overhang = config_get_int ("fet_diff_overhang");
-
-  default_load_cap = config_get_real ("default_load_cap");
-
-  extra_fet_string = config_get_string ("extra_fet_string");
-
-  disable_keepers = config_get_int ("disable_keepers");
-
-  act_cmdline = config_get_string ("act_cmdline");
-
-  discrete_length = config_get_int ("discrete_length");
-
-  swap_source_drain = config_get_int ("swap_source_drain");
-
-  use_subckt_models = config_get_int ("use_subckt_models");
-
-  fold_pfet_width = config_get_int ("fold_pfet_width");
-  fold_nfet_width = config_get_int ("fold_nfet_width");
-
-  if (config_exists ("width_limits")) {
-    int i;
-    fet_width_num = config_get_table_size ("width_limits");
-    if (fet_width_num % 2 == 1) {
-      fatal_error ("width limit table must contain an even number of entries");
-    }
-    if (fet_width_num > 0) {
-      fet_width_ranges = config_get_table_real ("width_limits");
-      for (i=0; i < fet_width_num-1; i++) {
-	if (fet_width_ranges[i] > fet_width_ranges[i+1]) {
-	  fatal_error ("width_limits: Range values must be monotonically increasing");
-	}
-      }
-      fet_width_num /= 2;
-    }
-  }
-
   if (!proc_name) {
     fprintf (stderr, "Missing process name.\n");
     usage ((*argv)[0]);
   }
 
+  act_cmdline = config_get_string ("act_cmdline");
+  
   /* create a new command line for ACT out of act_cmdline */
   int count = 1;
   
@@ -410,4 +270,45 @@ char *initialize_parameters (int *argc, char ***argv)
   (*argv)[2] = NULL;
 
   return proc_name;
+}
+
+
+
+int main (int argc, char **argv)
+{
+  Act *a;
+  char *proc;
+
+  proc = initialize_parameters (&argc, &argv);
+
+  if (argc != 2) {
+    fatal_error ("Something strange happened!");
+  }
+  if (proc == NULL) {
+    fatal_error ("Missing process name!");
+  }
+  
+  a = new Act (argv[1]);
+  a->Expand ();
+  a->mangle (config_get_string ("mangle_chars"));
+
+  Process *p = a->findProcess (proc);
+
+  if (!p) {
+    fatal_error ("Could not find process `%s' in file `%s'", proc, argv[1]);
+  }
+
+  if (!p->isExpanded()) {
+    fatal_error ("Process `%s' is not expanded.", proc);
+  }
+
+  act_prs_to_netlist (a, p);
+  act_create_bool_ports (a, p);
+  act_emit_netlist (a, p, fpout);
+
+  if (emit_verilog || emit_pinfo) {
+    emit_verilog_pins (a, fvout, fpinfo, p);
+  }
+
+  return 0;
 }
