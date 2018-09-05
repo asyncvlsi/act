4b561d90e2b8f4580dddf0c69b5f2847c90ef8aa
Rajit Manohar <rajit.manohar@yale.edu>
2018-09-05 06:54:11 -0400

    lint file config changed; initial template for prs2stk

---
+++ transform/prs2stk/main.cc
@@ -0,0 +1,53 @@
+/*************************************************************************
+ *
+ *  Copyright (c) 2018 Rajit Manohar
+ *  All Rights Reserved
+ *
+ **************************************************************************
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <act/act.h>
+#include <act/netlist.h>
+#include "config.h"
+
+
+static void usage (char *name)
+{
+  fprintf (stderr, "Usage: %s <actfile> <process> <stkfile>\n");
+  exit (1);
+}
+
+
+int main (int argc, char **argv)
+{
+  Act *a;
+  char *proc;
+
+  Act::Init (&argc, &argv);
+
+  if (argc != 4) {
+    usage (argv[0]);
+  }
+
+  a = new Act (argv[1]);
+  a->Expand ();
+  config_read ("prs2net.conf");
+
+  Process *p = a->findProcess (argv[2]);
+
+  if (!p) {
+    fatal_error ("Could not find process `%s' in file `%s'", argv[2], argv[1]);
+  }
+
+  if (!p->isExpanded()) {
+    fatal_error ("Process `%s' is not expanded.", argv[2]);
+  }
+
+  act_prs_to_netlist (a, p);
+
+  /* stack generation! */
+
+  return 0;
+}
