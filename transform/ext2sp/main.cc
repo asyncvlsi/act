4a69ee1f0f896aa982bec1bc51f991fea9cc5184
Rajit Manohar <rajit.manohar@yale.edu>
2019-04-30 07:13:39 -0400

    added area/perim printing extract inferred globals some more comments

--- transform/ext2sp/main.cc
+++ transform/ext2sp/main.cc
@@ -25,12 +25,26 @@
 #include <string.h>
 #include "ext.h"
 #include "hash.h"
+#include "array.h"
 #include "misc.h"
 
 double mincap = 0.1e-15;
 const char *gnd_node = "GND";
 
 #define SEP_CHAR ':'
+
+L_A_DECL (char *, globals);
+
+
+void addglobal (char *name)
+{
+  for (int i=0; i < A_LEN (globals); i++) {
+    if (strcmp (globals[i], name) == 0) return;
+  }
+  A_NEW (globals, char *);
+  A_NEXT (globals) = Strdup (name);
+  A_INC (globals);
+}
 
 char *name_munge (const char *name)
 {
@@ -85,6 +99,7 @@ struct alias_tree {
   struct alias_tree *up;
   char *name;
   double cap_gnd;
+  double p_perim, p_area, n_perim, n_area;
 };
 
 
@@ -148,6 +163,10 @@ struct alias_tree *getname (struct Hashtable *N, const char *name)
     a->up = NULL;
     a->name = name_munge (b->key);
     a->cap_gnd = 0;
+    a->n_area = 0;
+    a->n_perim = 0;
+    a->p_area = 0;
+    a->p_perim = 0;
     b->v = a;
     l = strlen (b->key);
     while (l > 0) {
@@ -170,6 +189,7 @@ struct alias_tree *getname (struct Hashtable *N, const char *name)
 
       if (s == 0 && a->name[e+1] == '\0') {
 	a->global = 2;
+	addglobal (a->name);
       }
       else {
 	MALLOC (tmp, char, e-s+2);
@@ -184,12 +204,14 @@ struct alias_tree *getname (struct Hashtable *N, const char *name)
 	  NEW (x, struct alias_tree);
 	  x->global = 2;
 	  x->name = g->key;
+	  addglobal (x->name);
 	  x->cap_gnd = 0;
 	  x->up = NULL;
 	  g->v = x;
 	}
 	x = (struct alias_tree *)g->v;
 	a->up = x;
+	FREE (tmp);
       }
     }
   }
@@ -229,9 +251,8 @@ void mergealias (struct alias_tree *a1, struct alias_tree *a2)
     }
   }
 }
-    
 
-void ext2spice (const char *name, struct ext_file *E)
+void ext2spice (const char *name, struct ext_file *E, int toplevel)
 {
   hash_bucket_t *b;
   int l;
@@ -245,20 +266,32 @@ void ext2spice (const char *name, struct ext_file *E)
   b = hash_add (seen, name);
 
   for (struct ext_list *lst = E->subcells; lst; lst = lst->next) {
-    ext2spice (lst->file, lst->ext);
+    ext2spice (lst->file, lst->ext, 0);
   }
 
-  l = strlen (name);
-  if (l >= 4 && (strcmp (name + l - 4, ".ext") == 0)) {
-    l -= 4;
+  if (!toplevel) {
+    l = strlen (name);
+    if (l >= 4 && (strcmp (name + l - 4, ".ext") == 0)) {
+      l -= 4;
+    }
+    printf ("*---------------------------------------------------\n");
+    printf ("* Subcircuit from %s\n", name);
+    printf ("*---------------------------------------------------\n");
+    printf (".subckt ");
+    while (l) {
+      putchar (*name);
+      name++;
+      l--;
+    }
+    printf (" _\n");
   }
-  printf (".subckt ");
-  while (l) {
-    putchar (*name);
-    name++;
-    l--;
+  else {
+    printf ("*\n");
+    printf ("*---------------------------------------------------\n");
+    printf ("*  Main extract file %s\n", name);
+    printf ("*---------------------------------------------------\n");
+    printf ("*\n");
   }
-  printf (" _\n");
 
   /*-- create names table --*/
   N = hash_new (32);
@@ -270,119 +303,163 @@ void ext2spice (const char *name, struct ext_file *E)
     t2 = getname (N, a->n2);
     mergealias (t1, t2);
   }
+
+  /*-- process area/perim --*/
+  for (struct ext_ap *a = E->ap; a; a = a->next) {
+    struct alias_tree *t = getname (N, a->node);
+    t->n_area += a->n_area;
+    t->n_perim += a->n_perim;
+    t->p_area += a->p_area;
+    t->p_perim += a->p_perim;
+  }
+  
   /*--- now print out aliases ---*/
-  printf ("* -- connections ---\n");
-  for (int i=0; i < N->size; i++) {
-    for (hash_bucket_t *b = N->head[i]; b; b = b->next) {
-      struct alias_tree *t = (struct alias_tree *)b->v;
-      if (t->up) {
-	printf ("V%d %s ", devcount++, t->name);
-	if (0 && (t->up->global == 1)) {
-	  int s, e;
-	  global_name (t->up->name, &s, &e);
-	  print_substr (t->up->name, s, e);
+  if (N->n > 0) {
+    printf ("* -- connections ---\n");
+    for (int i=0; i < N->size; i++) {
+      for (hash_bucket_t *b = N->head[i]; b; b = b->next) {
+	struct alias_tree *t = (struct alias_tree *)b->v;
+	if (t->up) {
+	  printf ("V%d %s ", devcount++, t->name);
+	  if (0 && (t->up->global == 1)) {
+	    int s, e;
+	    global_name (t->up->name, &s, &e);
+	    print_substr (t->up->name, s, e);
+	  }
+	  else {
+	    printf ("%s", t->up->name);
+	  }
+	  printf ("\n");
 	}
-	else {
-	  printf ("%s", t->up->name);
-	}
-	printf ("\n");
       }
     }
   }
 
   /*--- now print out fets ---*/
-  printf ("* -- fets ---\n");
-  for (struct ext_fets *fl = E->fet; fl; fl = fl->next) {
-    struct alias_tree *t;
-    printf ("M%d ", devcount++);
-    t = getname (N, fl->t2);
-    printf ("%s ", t->name);
-    t = getname (N, fl->g);
-    printf ("%s ", t->name);
-    t = getname (N, fl->t1);
-    printf ("%s ", t->name);
-    t = getname (N, fl->sub);
-    printf ("%s ", t->name);
-    if (fl->type == EXT_FET_PTYPE) {
-      printf ("pfet ");
+  if (E->fet) {
+    printf ("* -- fets ---\n");
+    for (struct ext_fets *fl = E->fet; fl; fl = fl->next) {
+      struct alias_tree *tsrc, *tdrain, *t;
+      printf ("M%d ", devcount++);
+      tdrain = getname (N, fl->t2); /* drain */
+      printf ("%s ", tdrain->name); /* gate */
+      t = getname (N, fl->g);  /* src */
+      printf ("%s ", t->name);
+      tsrc = getname (N, fl->t1);
+      printf ("%s ", tsrc->name);
+      t = getname (N, fl->sub);
+      printf ("%s ", t->name);
+      if (fl->type == EXT_FET_PTYPE) {
+	printf ("pfet ");
+      }
+      else {
+	printf ("nfet ");
+      }
+      printf ("W=%gU L=%gU", fl->width*1e6, fl->length*1e6);
+      if (fl->type == EXT_FET_PTYPE) {
+	printf ("\n+ AS=%gP PS=%gU", tsrc->p_area*1e12, tsrc->p_perim*1e6);
+	tsrc->p_perim = 0;
+	tsrc->p_area = 0;
+	printf (" AD=%gP PD=%gU", tdrain->p_area*1e12, tdrain->p_perim*1e6);
+	tdrain->p_perim = 0;
+	tdrain->p_area = 0;
+      }
+      else {
+	printf ("\n+ AS=%gP PS=%gU", tsrc->n_area*1e12, tsrc->n_perim*1e6);
+	tsrc->n_perim = 0;
+	tsrc->n_area = 0;
+	printf (" AD=%gP PD=%gU", tdrain->n_area*1e12, tdrain->n_perim*1e6);
+	tdrain->n_perim = 0;
+	tdrain->n_area = 0;
+      }
+      printf ("\n");
     }
-    else {
-      printf ("nfet ");
-    }
-    printf ("W=%gU L=%gU\n", fl->width*1e6, fl->length*1e6);
   }
 
   /*-- caps --*/
-  printf ("* -- caps ---\n");
+  if (E->cap) {
+    printf ("* -- caps ---\n");
 
-  /*--- collect cap to GND ---*/
-  for (struct ext_cap *l = E->cap; l; l = l->next) {
-    struct alias_tree *t, *u;
+    /*--- collect cap to GND ---*/
+    for (struct ext_cap *l = E->cap; l; l = l->next) {
+      struct alias_tree *t, *u;
 
-    t = getname (N, l->n1);
-    if (l->type == CAP_GND || l->type == CAP_SUBSTRATE) {
-      if (l->type == CAP_GND) {
-	t->cap_gnd += l->cap;
+      t = getname (N, l->n1);
+      if (l->type == CAP_GND || l->type == CAP_SUBSTRATE) {
+	if (l->type == CAP_GND) {
+	  t->cap_gnd += l->cap;
+	}
+      }
+      else {
+	u = getname (N, l->n2);
+	if (strcmp (t->name, gnd_node) == 0) {
+	  u->cap_gnd += l->cap;
+	}
+	else if (strcmp (u->name, gnd_node) == 0) {
+	  t->cap_gnd += l->cap;
+	}
       }
     }
-    else {
+      
+    for (struct ext_cap *l = E->cap; l; l = l->next) {
+      struct alias_tree *t, *u;
+
+      if (l->type == CAP_GND || l->type == CAP_SUBSTRATE) continue;
+    
+      t = getname (N, l->n1);
       u = getname (N, l->n2);
       if (strcmp (t->name, gnd_node) == 0) {
-	u->cap_gnd += l->cap;
+	continue;
       }
       else if (strcmp (u->name, gnd_node) == 0) {
-	t->cap_gnd += l->cap;
+	continue;
       }
-    }
-  }
-      
-  for (struct ext_cap *l = E->cap; l; l = l->next) {
-    struct alias_tree *t, *u;
 
-    if (l->type == CAP_GND || l->type == CAP_SUBSTRATE) continue;
+      if (l->cap < mincap) continue;
     
-    t = getname (N, l->n1);
-    u = getname (N, l->n2);
-    if (strcmp (t->name, gnd_node) == 0) {
-      continue;
-    }
-    else if (strcmp (u->name, gnd_node) == 0) {
-      continue;
+      printf ("C%d %s %s %gF\n", devcount++, t->name, u->name, l->cap*1.0e15);
     }
 
-    if (l->cap < mincap) continue;
-    
-    printf ("C%d %s %s %gF\n", devcount++, t->name, u->name, l->cap*1.0e15);
-  }
-
-  /* print caps to GND */
-  for (int i=0; i < N->size; i++) {
-    for (hash_bucket_t *b = N->head[i]; b; b = b->next) {
-      struct alias_tree *t = (struct alias_tree *)b->v;
-      if (t->cap_gnd > 0 && t->cap_gnd >= mincap) {
-	if (strcmp (t->name, gnd_node) == 0) continue;
-	printf ("C%d %s %s %gF\n", devcount++, t->name,
-		gnd_node, t->cap_gnd*1.0e15);
+    /* print caps to GND */
+    for (int i=0; i < N->size; i++) {
+      for (hash_bucket_t *b = N->head[i]; b; b = b->next) {
+	struct alias_tree *t = (struct alias_tree *)b->v;
+	if (t->cap_gnd > 0 && t->cap_gnd >= mincap) {
+	  if (strcmp (t->name, gnd_node) == 0) continue;
+	  printf ("C%d %s %s %gF\n", devcount++, t->name,
+		  gnd_node, t->cap_gnd*1.0e15);
+	}
       }
     }
   }
 
-  printf ("*--- subcircuits ---\n");
-  for (struct ext_list *lst = E->subcells; lst; lst = lst->next) {
-    int p = 0;
-    printf ("x%s %s ", lst->id, gnd_node);
-    l = strlen (lst->file);
-    if (l >= 4 && (strcmp (lst->file + l - 4, ".ext") == 0)) {
-      l -= 4;
+  if (E->subcells) {
+    printf ("*--- subcircuits ---\n");
+    for (struct ext_list *lst = E->subcells; lst; lst = lst->next) {
+      int p = 0;
+      printf ("x%s %s ", lst->id, gnd_node);
+      l = strlen (lst->file);
+      if (l >= 4 && (strcmp (lst->file + l - 4, ".ext") == 0)) {
+	l -= 4;
+      }
+      while (l) {
+	putchar (lst->file[p++]);
+	l--;
+      }
+      putchar ('\n');
     }
-    while (l) {
-      putchar (lst->file[p++]);
-      l--;
-    }
-    putchar ('\n');
   }
-  
-  printf (".ends\n");
+
+  if (!toplevel) {
+    printf (".ends\n");
+  }
+  else {
+    /* print globals! */
+    printf ("*--- inferred globals\n");
+    for (int i=0; i < A_LEN (globals); i++) {
+      printf (".global %s\n", globals[i]);
+    }
+  }
 
   /*--- delete hashtable ---*/
   for (int i=0; i < N->size; i++) {
@@ -401,6 +478,8 @@ int main (int argc, char **argv)
   extern int optind, opterr;
   extern char *optarg;
   struct ext_file *E;
+
+  A_INIT (globals);
 
   while ((ch = getopt (argc, argv, "c:")) != -1) {
     switch (ch) {
@@ -421,7 +500,7 @@ int main (int argc, char **argv)
   seen = hash_new (4);
   ext_validate_timestamp (argv[optind]);
   E = ext_read (argv[optind]);
-  ext2spice (argv[optind], E);
+  ext2spice (argv[optind], E, 1);
 
   return 0;
 }
