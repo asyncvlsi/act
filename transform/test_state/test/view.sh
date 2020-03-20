a2d433b3767979bf3e2142ca626259dc32922e69
Rajit Manohar <rajit.manohar@yale.edu>
2020-03-20 12:49:21 -0400

    test program for running the state pass added

---
+++ transform/test_state/test/view.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ARCH=`$VLSI_TOOLS_SRC/scripts/getarch`
+OS=`$VLSI_TOOLS_SRC/scripts/getos`
+EXT=${ARCH}_${OS}
+ACTTOOL=../test_statepass.$EXT 
+
+if [ $# -eq 0 ]
+then
+	list=*.act
+else
+	list="$@"
+fi
+
+if [ ! -d runs ]
+then
+	mkdir runs
+fi
+
+for i in $list
+do
+	echo "***** $i *****"
+	echo
+	cat $i
+	echo "--------------"
+	cat runs/$i.t.stdout
+	echo "--------------"
+	cat runs/$i.t.stderr
+	echo "*******"
+	echo
+done
