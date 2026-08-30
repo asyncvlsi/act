d4ee669b372590a884b2df2258bcaf2d7882c8fd
Rajit Manohar <rajit.manohar@yale.edu>
2026-08-30 06:59:47 -0400

    adjusted test case format

---
+++ transform/testing/reelab/test/validate.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ARCH=`$VLSI_TOOLS_SRC/scripts/getarch`
+OS=`$VLSI_TOOLS_SRC/scripts/getos`
+EXT=${ARCH}_${OS}
+if [ ! x$ACT_TEST_INSTALL = x ] || [ ! -f ../test_reelab.$EXT ]; then
+  ACTTOOL=$ACT_HOME/bin/test_arbpass
+  echo "testing installation"
+echo
+else
+  ACTTOOL=../test_reelab.$EXT
+fi
+
+if [ $# -eq 0 ]
+then
+	list=*.act
+else
+	list="$@"
+fi
+
+if [ ! -d runs ]
+then
+	mkdir runs
+fi
+
+for i in $list
+do
+	$ACTTOOL $i  > runs/$i.stdout 2> runs/$i.tmp.stderr
+	sort runs/$i.tmp.stderr > runs/$i.stderr
+	rm runs/$i.tmp.stderr
+done
